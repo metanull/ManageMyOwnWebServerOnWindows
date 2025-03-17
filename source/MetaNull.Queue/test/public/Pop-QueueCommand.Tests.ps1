359bbb55aadf070878b477737e3f12b50f9a48d3
Pascal HAVELANGE <havelangep@hotmail.com>
2025-03-17 17:32:42 +0100

    MetaNull.Queue v0.0.72.0 - Pester Tests

---
+++ source/MetaNull.Queue/test/public/Pop-QueueCommand.Tests.ps1
@@ -0,0 +1,98 @@
+Describe "Pop-QueueCommand" -Tag "Functional","BeforeBuild" {
+
+    Context "When the function is called" {
+        
+        BeforeAll {
+            # Load TestData
+            . (Join-Path (Split-Path $PSCommandPath) "TestData.ps1")
+
+            # Initialize tests (get references to Module Function's Code)
+            $ModuleRoot = $PSCommandPath | Split-Path -Parent | Split-Path -Parent | Split-Path -Parent
+            $ScriptName = $PSCommandPath | Split-Path -Leaf
+            $Visibility = $PSCommandPath | Split-Path -Parent | Split-Path -Leaf
+            $SourceDirectory = Resolve-Path (Join-Path $ModuleRoot "source\$Visibility")
+            $TestDirectory = Resolve-Path (Join-Path $ModuleRoot "test\$Visibility")
+
+            # Create a Stub for the module function to test
+            $FunctionPath = Join-Path $SourceDirectory ($ScriptName -replace '\.Tests\.ps1$', '.ps1')
+            Function Invoke-ModuleFunctionStub {
+                . $FunctionPath @args | write-Output
+            }
+        }
+        AfterAll {
+            # Cleanup (remove the whole test registry key)
+            Remove-Item -Force -Recurse -Path MetaNull:\ -ErrorAction SilentlyContinue  | Out-Null
+            Remove-PSDrive -Name MetaNull -Scope Script -ErrorAction SilentlyContinue
+        }
+        BeforeEach {
+            # Adding test data to the registry
+            $TestData | Foreach-Object {
+                $Id = $_.Queue.Id
+                $Properties = $_.Queue
+                New-Item "MetaNull:\Queues\$Id\Commands" -Force | Out-Null
+                $Item = Get-Item "MetaNull:\Queues\$Id"
+                $Properties.GetEnumerator() | ForEach-Object {
+                    $Item | New-ItemProperty -Name $_.Key -Value $_.Value | Out-Null
+                }
+                $_.Commands | Foreach-Object {
+                    $Item = New-Item -Path "MetaNull:\Queues\$Id\Commands\$($_.Index)" -Force
+                    $_.GetEnumerator() | ForEach-Object {
+                        $Item | New-ItemProperty -Name $_.Key -Value $_.Value | Out-Null
+                    }
+                }
+            }
+        }
+        AfterEach {
+            # Cleanup (remove all queues)
+            Remove-Item -Force -Recurse -Path MetaNull:\Queues\* -ErrorAction SilentlyContinue  | Out-Null
+        }
+
+        It "Test environment PSDrive should be initialized" {
+            $MetaNull.Queue.Drive | Should -Not -BeNullOrEmpty
+            {Get-PSDrive -Name 'MetaNull'} | Should -Not -Throw
+            Get-PSDrive -Name 'MetaNull' | Should -Not -BeNullOrEmpty
+            Test-Path MetaNull:\Queues | Should -BeTrue
+        }
+        It "Test environment should be initialized and test queue exists" {
+            $TestData | Foreach-Object {
+                $Queue = $_.Queue
+                Test-Path "MetaNull:\Queues\$($Queue.Id)\Commands" | Should -BeTrue
+                (Get-ChildItem "MetaNull:\Queues\$($Queue.Id)\Commands").Count | Should -Be $($_.Commands.Count)
+                $_.Commands | Foreach-Object {
+                    Test-Path "MetaNull:\Queues\$($Queue.Id)\Commands\$($_.Index)" | Should -BeTrue
+                }
+            }
+        }
+        It "Should not throw an exception" {
+            $TestData | Foreach-Object {
+                $Queue = $_.Queue
+                {$Command = Invoke-ModuleFunctionStub -Id $Queue.Id} | Should -Not -Throw
+            }
+        }
+        It "Should pop the last command from the registry" {
+            $TestData | Foreach-Object {
+                $Queue = $_.Queue
+                $Index = $_.Commands.Index | Select-Object -Last 1
+                $Command = Invoke-ModuleFunctionStub -Id $Queue.Id
+                Test-Path "MetaNull:\Queues\$($Queue.Id)\Commands\$Index" | Should -BeFalse
+            }
+        }
+        It "Should unshift the first command from the registry" {
+            $TestData | Foreach-Object {
+                $Queue = $_.Queue
+                $Index = $_.Commands.Index | Select-Object -First 1
+                $Command = Invoke-ModuleFunctionStub -Id $Queue.Id -Unshift
+                Test-Path "MetaNull:\Queues\$($Queue.Id)\Commands\$Index" | Should -BeFalse
+            }
+        }
+        It "Should return the popped command from the registry" {
+            $TestData | Foreach-Object {
+                $Queue = $_.Queue
+                $Index = $_.Commands.Index | Select-Object -First 1
+                $Command = Invoke-ModuleFunctionStub -Id $Queue.Id -Unshift
+                $Command.Index | Should -Be $Index
+                $Command.Command | Should -Be ($_.Commands | Where-Object { $_.Index -eq $Index} | Select-Object -ExpandProperty Command) 
+            }
+        }
+    }
+}
---
+++ source/MetaNull.Queue/test/public/Push-QueueCommand.Tests.ps1
@@ -0,0 +1,99 @@
+Describe "Push-QueueCommand" -Tag "Functional","BeforeBuild" {
+
+    Context "When the function is called" {
+        
+        BeforeAll {
+            # Load TestData
+            . (Join-Path (Split-Path $PSCommandPath) "TestData.ps1")
+
+            # Initialize tests (get references to Module Function's Code)
+            $ModuleRoot = $PSCommandPath | Split-Path -Parent | Split-Path -Parent | Split-Path -Parent
+            $ScriptName = $PSCommandPath | Split-Path -Leaf
+            $Visibility = $PSCommandPath | Split-Path -Parent | Split-Path -Leaf
+            $SourceDirectory = Resolve-Path (Join-Path $ModuleRoot "source\$Visibility")
+            $TestDirectory = Resolve-Path (Join-Path $ModuleRoot "test\$Visibility")
+
+            # Create a Stub for the module function to test
+            $FunctionPath = Join-Path $SourceDirectory ($ScriptName -replace '\.Tests\.ps1$', '.ps1')
+            Function Invoke-ModuleFunctionStub {
+                . $FunctionPath @args | write-Output
+            }
+        }
+        AfterAll {
+            # Cleanup (remove the whole test registry key)
+            Remove-Item -Force -Recurse -Path MetaNull:\ -ErrorAction SilentlyContinue  | Out-Null
+            Remove-PSDrive -Name MetaNull -Scope Script -ErrorAction SilentlyContinue
+        }
+        BeforeEach {
+            # Adding test data to the registry
+            $TestData | Foreach-Object {
+                $Id = $_.Queue.Id
+                $Properties = $_.Queue
+                New-Item "MetaNull:\Queues\$Id\Commands" -Force | Out-Null
+                $Item = Get-Item "MetaNull:\Queues\$Id"
+                $Properties.GetEnumerator() | ForEach-Object {
+                    $Item | New-ItemProperty -Name $_.Key -Value $_.Value | Out-Null
+                }
+                $_.Commands | Foreach-Object {
+                    $Item = New-Item -Path "MetaNull:\Queues\$Id\Commands\$($_.Index)" -Force
+                    $_.GetEnumerator() | ForEach-Object {
+                        $Item | New-ItemProperty -Name $_.Key -Value $_.Value | Out-Null
+                    }
+                }
+            }
+        }
+        AfterEach {
+            # Cleanup (remove all queues)
+            Remove-Item -Force -Recurse -Path MetaNull:\Queues\* -ErrorAction SilentlyContinue  | Out-Null
+        }
+
+        It "Test environment PSDrive should be initialized" {
+            $MetaNull.Queue.Drive | Should -Not -BeNullOrEmpty
+            {Get-PSDrive -Name 'MetaNull'} | Should -Not -Throw
+            Get-PSDrive -Name 'MetaNull' | Should -Not -BeNullOrEmpty
+            Test-Path MetaNull:\Queues | Should -BeTrue
+        }
+        It "Test environment should be initialized and test queue exists" {
+            $TestData | Foreach-Object {
+                $Queue = $_.Queue
+                Test-Path "MetaNull:\Queues\$($Queue.Id)\Commands" | Should -BeTrue
+                (Get-ChildItem "MetaNull:\Queues\$($Queue.Id)\Commands").Count | Should -Be $($_.Commands.Count)
+                $_.Commands | Foreach-Object {
+                    Test-Path "MetaNull:\Queues\$($Queue.Id)\Commands\$($_.Index)" | Should -BeTrue
+                }
+            }
+        }
+        It "Should not throw an exception" {
+            $TestData | Foreach-Object {
+                $Queue = $_.Queue
+                {$Index = Invoke-ModuleFunctionStub -Id $Queue.Id -Name 'Test-66' -Command 'Test-66'} | Should -Not -Throw
+            }
+        }
+        It "Should add the command to the registry" {
+            $TestData | Foreach-Object {
+                $Queue = $_.Queue
+                $Index = Invoke-ModuleFunctionStub -Id $Queue.Id -Name 'Test-72' -Command 'Test-72'
+                $Index | Should -Not -BeNullOrEmpty
+                $Item = Get-Item -Path "MetaNull:\Queues\$($Queue.Id)\Commands\$Index"
+                $Item | Should -Not -BeNullOrEmpty
+                $Item | Get-ItemPropertyValue -Name 'Command' | Should -Be 'Test-72'
+                $Item | Get-ItemPropertyValue -Name 'Index' | Should -Be $Index
+            }
+        }
+        It "Should increment the Index automatically" {
+            $TestData | Foreach-Object {
+                $Queue = $_.Queue
+                $Index1 = Invoke-ModuleFunctionStub -Id $Queue.Id -Name 'Test-85.1' -Command 'Test-85.1'
+                $Index2 = Invoke-ModuleFunctionStub -Id $Queue.Id -Name 'Test-85.2' -Command 'Test-85.2'
+                $Index3 = Invoke-ModuleFunctionStub -Id $Queue.Id -Name 'Test-85.3' -Command 'Test-85.3'
+                $Index1 | Should -Not -BeNullOrEmpty
+                $Index2 | Should -Not -BeNullOrEmpty
+                $Index3 | Should -Not -BeNullOrEmpty
+                $Index1 | Should -Not -Be $Index3
+                $Index2 | Should -Be ($Index1 + 1)
+                $Index3 | Should -Be ($Index2 + 1)
+            }
+        }
+        
+    }
+}
